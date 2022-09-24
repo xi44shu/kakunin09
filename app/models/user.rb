b80c8a6ef1c7254426482862c07cafe26763875e
joni-deppu <44shuxichuan@gmail.com>
2022-09-24 22:35:33 +0900

    user管理機能実装

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: ' は有効でありません。' }
+  validates :admin, inclusion: {in: [true, false]}
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+   name              { '田なカ' }
+   admin              { 'false' }
+   email                 {Faker::Internet.free_email}
+   password              {'1a' +Faker::Internet.password(min_length: 6)}
+   password_confirmation {password}
+  end
+ end
