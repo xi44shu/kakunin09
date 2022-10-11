3410427f67e1fa0085bd998b0be59318a5513794
joni-deppu <44shuxichuan@gmail.com>
2022-10-11 16:51:28 +0900

    TradingCompanyバリデーション修正

--- app/models/trading_company.rb
+++ app/models/trading_company.rb
@@ -1,6 +1,6 @@
 class TradingCompany < ApplicationRecord
   with_options presence: true do
-    validates :tc_name, :tc_contact_person, presence: true
+    validates :tc_name, :tc_contact_person
     validates :tc_telephone, format: { with: /\A[0-9]{10,11}\z/ }
   end
   has_many :schedules
